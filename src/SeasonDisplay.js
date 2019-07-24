3e1e7d070ccac9b668677547c0c33ccb98ce55e2
Elliot Burin <eburin2@gmail.com>
2019-07-24 14:57:50 -0700

    remove setState from constructor and add it to componetWillMount method in app component

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'Summer' : 'Winter';
+  } else {
+    return lat > 0 ? 'Winter' : 'Summer';
+  }
+}
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text = season === 'winter' ? "Burr, it's chilly" : "Let's hit the beach"
+  return (
+    <div><h1>{text}</h1></div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
 
-      // THIS IS THE ONLY TIME we do direct assigment to this.state
-      this.state = { lat: null, errorMessage: ''};
+  state = { lat: null, errorMessage: ''};
 
-      window.navigator.geolocation.getCurrentPosition(
-        (position) => {
-          this.setState({lat: position.coords.latitude});
-        },
-        (err) => {
-          this.setState({errorMessage: err.message});
-        }
-      );
-    }
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({lat: position.coords.latitude}),
+      (err) => this.setState({errorMessage: err.message})
+    );
+  }
+
 
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
+    } else if(!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />
+    } else {
+      return <div>Loading ...</div>;
     }
-
-    if(!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
-    }
-
-    return <div>Loading</div>;
   }
 }
+
+
 
 ReactDOM.render(<App />,  document.querySelector('#root'));
